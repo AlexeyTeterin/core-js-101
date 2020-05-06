0228fe655b0dd2ca6c16265ce4ec382f7249c289
AlexeyTeterin <teterin.alexey@gmail.com>
2020-05-06 14:58:26 +0500

    42 pass

--- src/06-objects-tasks.js
+++ src/06-objects-tasks.js
@@ -20,8 +20,13 @@
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(w, h) {
+  const rectangle = {
+    height: h,
+    width: w,
+    getArea: () => w * h,
+  };
+  return rectangle;
 }
 
 
